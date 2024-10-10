20a7dd13e4e0b1aaa3386e011e1e9ff2fad021a5
Brian <briankgathui@gmail.com>
2024-10-10 23:15:43 +0300

    Completing the Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
+// INDEX.JS
 // Write your code here
+
+// Define num1 and num2 for the multiply test
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Define random for the random test
+let random = Math.floor(Math.random() * 100) + 1;
+
+
+// Define num3 and num4 for the mod test
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+
+// Define max for the max test
+let max = Math.max(1, 5, 10, 20);
